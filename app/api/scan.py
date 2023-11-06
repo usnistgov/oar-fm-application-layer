55aec2d1b6714da085629e8c3b16f2756c7ed12c
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-11-06 11:37:41 -0500

    Update scan.py

--- app/api/scan.py
+++ app/api/scan.py
@@ -3,53 +3,96 @@
 /scan-status endpoint returns the status of async scan task
 """
 import uuid
-from multiprocessing import Process
+import json
+import asyncio
+import threading
 
+from helpers import parse_nextcloud_scan_xml, calculate_checksum
+import os
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
+from config import Config
+from abc import ABC, abstractmethod
 
 from app.utils import files
 
-scanning = {}
+# A dictionary to store the status of multiple tasks
+tasks_status = {}
 
 
-# Async task
-def scan_checksum(task_id):
-    checksum = 'I am a checksum'
-    # status types
-    status = 'completed'
+class DirectoryScanner(ABC):
 
-    scanning[task_id] = {
-        'status': status,
-        'checksum': checksum
-    }
+    @abstractmethod
+    def fast_scan(self, dir_path: str):
+        pass
 
-    return scanning[task_id]
+    @abstractmethod
+    async def slow_scan(self, dir_path: str):
+        pass
+
+
+class FileManagerDirectoryScanner(DirectoryScanner):
+
+    def fast_scan(self, dir_path: str):
+        scan_result = files.put_scandir(dir_path)
+        return parse_nextcloud_scan_xml(scan_result)
+
+    async def slow_scan(self, dir_path: str):
+        checksums = {}
+        for dirpath, dirnames, filenames in os.walk(dir_path):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
+                checksums[file_path] = calculate_checksum(file_path)
+        return checksums
 
 
 class ScanFiles(Resource):
     @jwt_required()
-    def put(self, user_name, record_name):
+    def put(self, record_name):
         try:
-            #TODO: make sure to NOT start a scan for a user-record if another one is already ongoing
+            # Instantiate dirs
+            parent_dir = f"mds2-{record_name}"
+            system_dir = f"{parent_dir}/mds2-{record_name}-sys"
+            record_space = f"{parent_dir}/mds2-{record_name}"
+
+            scanner = FileManagerDirectoryScanner()
+            fast_scan_data = scanner.fast_scan(record_space)
+
+            root_dir = Config.NEXTCLOUD_ROOT_DIR_PATH
+            system_dir = os.path.join(root_dir, system_dir)
+
+            filename = 'report.json'
+            filepath = os.path.join(system_dir, filename)
+
+            with open(filepath, 'w') as file:
+                json.dump({'nextcloud_scan': fast_scan_data}, file, indent=4)
+
             task_id = str(uuid.uuid4())
-            # run async
-            p = Process(target=scan_checksum(task_id))
-            p.start()
+            tasks_status[task_id] = {'Status': 'In Progress'}
+            record_fullpath = os.path.join(root_dir, record_space)
 
-            scan_user = files.put_scan(user_name)
+            # Run the slowScan asynchronously using a thread
+            def run_slow_scan():
+                checksums = asyncio.run(scanner.slow_scan(record_fullpath))
+                tasks_status[task_id]['Checksums'] = checksums
+                tasks_status[task_id]['Status'] = 'Completed'
 
-            direct_result = {
-                'scan_user': scan_user,
-                'task_id': task_id
-            }
+                # Update the report.json file after slow scan
+                report = {'nextcloud_scan': fast_scan_data, 'Checksums': checksums}
+                with open(filepath, 'w') as file:
+                    json.dump(report, file, indent=4)
+
+            thread = threading.Thread(target=run_slow_scan)
+            thread.start()
 
             success_response = {
                 'success': 'PUT',
-                'message': direct_result
+                'message': 'Scanning successfully started!',
+                'task_id': task_id
             }
 
-            return success_response, 201
+            return success_response, 200
+
         except Exception as error:
             error_response = {
                 'error': 'Bad Request',
@@ -59,21 +102,11 @@ class ScanFiles(Resource):
 
 
 class ScanStatus(Resource):
+
     @jwt_required()
     def get(self, task_id):
-        try:
-            scan_status = {}
-            result = scan_checksum(task_id)
-            scan_status['status'] = result['status']
-
-            if scan_status['status'] == 'completed':
-                scan_status['checksum'] = result['checksum']
-
-            return scan_status
-
-        except Exception as error:
-            error_response = {
-                'error': 'Bad Request',
-                'message': str(error)
-            }
-            return error_response, 400
+        task_status = tasks_status.get(task_id)
+        if task_status:
+            return task_status, 200
+        else:
+            return {'error': 'Not Found', 'message': 'Task ID not found!'}, 404
