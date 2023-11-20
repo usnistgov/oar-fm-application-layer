22834a4e2ecac150c9bbcad05c10f3b1aae51bfb
MelvinMartins-NIST <114179002+MelvinMartins-NIST@users.noreply.github.com>
2023-11-20 12:06:23 -0500

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -9,7 +9,7 @@ from app.api.auth import Authentication
 from app.api.permissions import Permissions
 from app.api.record_space import RecordSpace
 from app.api.file import File
-from app.api.scan import ScanFiles
+from app.api.scan import Scans
 from app.api.test import Test
 
 api_bp = Blueprint("api", __name__)
@@ -23,8 +23,10 @@ api.add_resource(RecordSpace,
 api.add_resource(File,
                  "/file",
                  "/file/<string:destination_path>")
-api.add_resource(ScanFiles, "/scans/<string:record_name>")
-api.add_resource(ScanFiles, "/scans/<string:scan_id>")
+api.add_resource(Scans, 
+                 "/scans/<string:record_name>",
+                 "/scans/<string:scan_id>"
+                )
 api.add_resource(Permissions,
                  "/permissions/<string:user_name>/<string:record_name>/<string:permission_type>",
                  "/permissions/<string:user_name>/<string:record_name>"
